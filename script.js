fcc3a892bdbb417f8f4cbc6429e9174e1d27a0b2
Huaptlo <114053742+Huaptlo@users.noreply.github.com>
2024-12-06 22:13:44 +0200

    again

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import { initializeApp } from "firebase/app";
+
 // Firebase configuration (replace with your Firebase project's config)
 const firebaseConfig = {
     apiKey: "AIzaSyDSWuVpts51J-Tx_-eseP42pLLjmB1e7KQ",
